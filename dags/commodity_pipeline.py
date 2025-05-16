ca489b6c922044b67ffb6b137c53970de62c074e
Loic <loictiems@yahoo.fr>
2025-05-15 22:13:59 -0400

    add commodity ingestion

---
+++ dags/commodity_pipeline.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+from src.ingestion.eia_api_ingest import fetch_eia_data
+from src.transformation.spark_transforms import transform_data
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+}
+
+with DAG(
+    'commodity_data_pipeline',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['commodity', 'ETL']
+) as dag:
+
+    ingest_task = PythonOperator(
+        task_id='ingest_eia_data',
+        python_callable=fetch_eia_data
+    )
+
+    transform_task = PythonOperator(
+        task_id='transform_data_spark',
+        python_callable=transform_data
+    )
+
+    ingest_task >> transform_task
--- dags/data_ingestion.py
+++ dags/data_ingestion.py
@@ -6,24 +6,21 @@ import pandas as pd
 import os
 
 def fetch_energy_data():
-    reponse = requests.get("https://api.eia.gov/example")
+    response = requests.get("https://api.eia.gov/example")
     data = response.json()
-    df = pd.DataFrame(data["Series"])
-    os.makedirs("/opt/airflow/data/raw", exist_ok = True)
-    df.to_csv("/opt/airflow/data/raw/eia.csv", index = False)
+    df = pd.DataFrame(data["series"])
+    os.makedirs("/opt/airflow/data/raw", exist_ok=True)
+    df.to_csv("/opt/airflow/data/raw/eia.csv", index=False)
 
 with DAG(
-    dag_id ="data_ingestion_pipeline",
-    start_data = datetime(2023,1,1),
-    schedule_interval= "daily",
-    catchup = False,
+    dag_id="data_ingestion_pipeline",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
 ) as dag:
     ingest_task = PythonOperator(
-        task_id ="fetch_energy_data",
+        task_id="fetch_energy_data",
         python_callable=fetch_energy_data,
-
-
-    )    
-
+    )
 
     ingest_task
