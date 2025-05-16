48b81bbe47b5e676a19f6012631d1f3e2cc7d2c7
Loic <loictiems@yahoo.fr>
2025-05-15 21:35:23 -0400

    Data Ingestion

--- dags/data_ingestion.py
+++ dags/data_ingestion.py
@@ -5,4 +5,25 @@ import requests
 import pandas as pd
 import os
 
-def fetch
+def fetch_energy_data():
+    reponse = requests.get("https://api.eia.gov/example")
+    data = response.json()
+    df = pd.DataFrame(data["Series"])
+    os.makedirs("/opt/airflow/data/raw", exist_ok = True)
+    df.to_csv("/opt/airflow/data/raw/eia.csv", index = False)
+
+with DAG(
+    dag_id ="data_ingestion_pipeline",
+    start_data = datetime(2023,1,1),
+    schedule_interval= "daily",
+    catchup = False,
+) as dag:
+    ingest_task = PythonOperator(
+        task_id ="fetch_energy_data",
+        python_callable=fetch_energy_data,
+
+
+    )    
+
+
+    ingest_task
